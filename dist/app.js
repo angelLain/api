1dc03b805a7a2a0bda3cfe39b306b7cdb224a970
angelLain <angelvillagaray1@gmail.com>
2023-02-16 10:36:11 -0500

    first commit

--- dist/app.js
+++ dist/app.js
@@ -1,42 +1,60 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var cors = require("cors");
-var path = require("path");
+const express_1 = __importDefault(require("express"));
+const cors = require("cors");
+const path = require("path");
 var html_to_pdf = require("html-pdf-node");
-var fs = require("fs");
-var app = (0, express_1.default)();
+const fs = require("fs");
+const app = (0, express_1.default)();
 var http = require("http");
-var PORT = process.env.PORT || 3977;
+const PORT = process.env.PORT || 3977;
 var server = http.createServer(app);
 app.use(cors({ origin: true }));
 app.use(express_1.default.json());
-server.listen(PORT, function () {
+server.listen(PORT, () => {
     console.log("puerto " + PORT);
 });
-app.post("/crear_pdf", function (req, res) {
-    var options = { format: "A4" };
-    console.log("pfg4654");
-    var file = { content: req.body.html };
-    html_to_pdf.generatePdf(file, options).then(function (pdfBuffer) {
-        // agregar el encabezado Access-Control-Allow-Origin a la respuesta
-        res.send(pdfBuffer);
-    }, function (err) {
-        res.send(err);
-    });
-});
-app.get("/prueba", function (req, res) {
+const puppeteer_1 = __importDefault(require("puppeteer"));
+app.get('/crear_pdf', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { html } = req.body.html;
+    if (!html) {
+        return res.status(400).send('Missing required parameter: html');
+    }
+    try {
+        const browser = yield puppeteer_1.default.launch();
+        const page = yield browser.newPage();
+        yield page.setContent(html);
+        const pdf = yield page.pdf();
+        yield browser.close();
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(pdf);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while generating the PDF');
+    }
+}));
+app.get("/prueba", (req, res) => {
     console.log("hola");
     res.status(200).send("hola mundo");
 });
-app.post("/prueba_2", function (req, res) {
+app.post("/prueba_2", (req, res) => {
     console.log("hola");
     res.status(200).send(req.body);
 });
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     console.log("hola");
     res.status(200).send("hola mundo");
 });
