b65861fdedecf9fdcbf0b2bfdcd8a58e7c8050c1
angelLain <angelvillagaray1@gmail.com>
2023-02-15 20:01:53 -0500

    first commit

---
+++ dist/app.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors = require("cors");
+var path = require("path");
+var html_to_pdf = require("html-pdf-node");
+var fs = require("fs");
+var app = (0, express_1.default)();
+var http = require("http");
+var server = http.createServer(app);
+server.listen(8000);
+app.use(express_1.default.json());
+app.get("/crear_pdf", function (req, res) {
+    console.log("adasd");
+    var options = { format: "A4" };
+    console.log("pfg4654");
+    var file = { content: req.body.html };
+    html_to_pdf.generatePdf(file, options).then(function (pdfBuffer) {
+        // agregar el encabezado Access-Control-Allow-Origin a la respuesta
+        res.send(pdfBuffer);
+    }, function (err) {
+        res.send(err);
+    });
+});
---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+const cors = require("cors");
+const path = require("path");
+var html_to_pdf = require("html-pdf-node");
+const fs = require("fs");
+const app = express();
+var http = require("http");
+var server = http.createServer(app);
+server.listen(8000);
+app.use(express.json());
+
+app.get("/crear_pdf", (req, res) => {
+  let options = { format: "A4" };
+  console.log("pfg4654");
+  let file = { content: req.body.html };
+  html_to_pdf.generatePdf(file, options).then(
+    (pdfBuffer: any) => {
+      // agregar el encabezado Access-Control-Allow-Origin a la respuesta
+      res.send(pdfBuffer);
+    },
+    (err: any) => {
+      res.send(err);
+    }
+  );
+});
